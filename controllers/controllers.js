2e3d422fe8e819f80c31059340f89c7d634e1c89
dean <iamdekanos@gmail.com>
2021-06-23 10:41:26 +0300

    added signup and login handlers and routes

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -1,3 +1,6 @@
 exports.getHome = (req, res) => res.render("home");
-
 exports.getSmoothies = (req, res) => res.render("smoothies");
+exports.getSignup = (req, res) => res.render("signup");
+exports.getLogin = (req, res) => res.render("login");
+exports.postSignup = (req, res) => res.send("new signup");
+exports.postLogin = (req, res) => res.send("new login");
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,10 @@
-const express = require("express");
-const router = express.Router();
-const { getHome, getSmoothies } = require("../controllers/controllers");
+const { Router } = require("express");
+const router = Router();
+const { getHome, getSmoothies, getSignup, getLogin, postLogin, postSignup } = require("../controllers/controllers");
 
 router.route("/").get(getHome);
 router.route("/smoothies").get(getSmoothies);
+router.route("/signup").get(getSignup).post(postSignup);
+router.route("/login").get(getLogin).post(postLogin);
 
 module.exports = router;
